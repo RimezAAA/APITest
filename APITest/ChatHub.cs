849c8e9eeb2bf234c9a7fc4d71034f9e92b26af1
Арсений Анохин <artuhi.arsenii@gmail.com>
2023-01-23 18:50:56 +0300

    Добавьте файлы проекта.

---
+++ APITest/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace APITest
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string userName, string email)
+        {
+            await Clients.All.SendAsync("Receive", message, userName, email);
+        }
+    }
+}
